181820caacdf606f2f4c7aaae1297129609d97a2
dsklingerman <dklinger@uncc.edu>
2017-10-11 13:26:24 +0000

    Done with section 4.7

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,10 @@
 class MoviesController < ApplicationController
+    def create
+        #@movie = Movie.create!(params[:movie]) #old way
+        @movie = Movie.create!(movie_params)  # new way
+        flash[:notice] = "#{@movie.title} was successfully created."
+        redirect_to movies_path
+    end
     def index
         @movies = Movie.all
     end
